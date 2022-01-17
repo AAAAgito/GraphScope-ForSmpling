bded77c191db3d0083890bb9c0abe8260a297489
melo <lianyixiaofan@foxmail.com>
2022-01-17 17:25:18 +0800

    teach_examples

---
+++ research/gaia-x/gremlin/gremlin_core/src/bin/teach_examples.rs
@@ -0,0 +1,175 @@
+use graph_store::config::GraphDBConfig;
+use graph_store::graph_db::GlobalStoreTrait;
+use graph_store::graph_db_impl::LargeGraphDB;
+use gremlin_core::graph_proxy::{create_demo_graph, GRAPH};
+use gremlin_core::structure::GraphElement;
+use pegasus::api::{Count, Filter, Fold, KeyBy, Map, PartitionByKey, Sink, Join, Dedup};
+use pegasus::result::ResultStream;
+use pegasus::{Configuration, JobConf, flat_map, ServerConf};
+use strum_macros::ToString;
+use core::time;
+use std::path::PathBuf;
+use std::slice::SliceIndex;
+use std::sync::Arc;
+use std::vec;
+use std::collections::HashSet;
+use structopt::StructOpt;
+
+use std::fmt::Debug;
+use pegasus::errors::{BuildJobError, JobSubmitError, SpawnJobError, StartupError};
+use graph_store::graph_db::Direction;
+
+use std::time::Instant;
+
+#[derive(Debug, Clone, StructOpt, Default)]
+pub struct Config {
+    #[structopt(short = "s", long = "servers")]
+    servers: Option<PathBuf>,
+    #[structopt(short = "w", long = "workers", default_value = "1")]
+    workers: u32,
+    #[structopt(short = "b", long = "benchmark", default_value = "t")]
+    benchmark_type: String
+}
+
+fn main() {
+    pegasus_common::logs::init_log();
+    let config: Config = Config::from_args();
+    let server_conf = if let Some(ref servers) = config.servers {
+        let servers = std::fs::read_to_string(servers).unwrap();
+        Configuration::parse(&servers).unwrap()
+    } else {
+        Configuration::singleton()
+    };
+    pegasus::startup(server_conf).unwrap();
+    let mut conf = JobConf::new("example");
+    conf.set_workers(config.workers);
+
+    if config.servers.is_some() {
+        conf.reset_servers(ServerConf::All);
+    }
+
+    pegasus::wait_servers_ready(conf.servers());
+
+    create_demo_graph();
+
+    let mut result = _teach_example5(conf).expect("Run Job Error!");
+    
+    while let Some(Ok(data)) = result.next() {
+        println!("{:?}", data);
+    }
+    
+    pegasus::shutdown_all();
+}
+
+fn _teach_example1(conf: JobConf) -> Result<ResultStream<u64>, JobSubmitError> {
+    pegasus::run(conf, move || {
+        move |input, output| {
+            input.input_from(GRAPH
+                .get_all_vertices(None)
+                .map(|v| (v.get_id() as u64)))?
+            .sink_into(output)
+        }
+    })
+}
+
+fn _teach_example2(conf: JobConf) -> Result<ResultStream<u64>, JobSubmitError> {
+    pegasus::run(conf, move || {
+        move |input, output| {
+            input.input_from(GRAPH.get_all_vertices(None).map(|v| (v.get_id() as u64)))?
+            .flat_map(|v| {
+                let adj_vertices = GRAPH
+                    .get_adj_vertices(v as usize, None, Direction::Outgoing)
+                    .map(|vertex| vertex.get_id() as u64);
+                Ok(adj_vertices)
+            })?
+            .sink_into(output)
+        }
+    })
+}
+
+fn _teach_example3(conf: JobConf) -> Result<ResultStream<Vec<u64>>, JobSubmitError> {
+    pegasus::run(conf, move || {
+        move |input, output| {
+            input.input_from(GRAPH.get_all_vertices(None).map(|v| (v.get_id() as u64)))?
+            .flat_map(|v_id| {
+                let label_ids = vec![1];
+                let adj_vertices = GRAPH.get_adj_vertices(v_id as usize, Some(&label_ids), Direction::Outgoing);
+                Ok(adj_vertices.map(move |v| {
+                    let mut path = vec![];
+                    path.push(v_id);
+                    path.push(v.get_id() as u64);
+                    path
+                }))
+            })?
+            .flat_map(|path| {
+                let extend_item_id = path[1];
+                let e_label_ids = vec![1];
+                let adj_vectices = GRAPH.get_adj_vertices(extend_item_id as usize, Some(&e_label_ids), Direction::Incoming);
+                Ok(adj_vectices.map(move |v| {
+                    let mut new_path = path.clone();
+                    new_path.push(v.get_id() as u64);
+                    new_path
+                }))
+            })?
+            .sink_into(output)
+        }
+    })
+}
+
+fn _teach_example4(conf: JobConf) -> Result<ResultStream<Vec<u64>>, JobSubmitError> {
+    pegasus::run(conf, move || {
+        move |input, output| {
+            let left_partition = input.input_from(GRAPH.get_all_vertices(None).map(|v| (v.get_id() as u64)))?
+            .flat_map(|v_id| {
+                let label_ids = vec![1];
+                let adj_vertices = GRAPH.get_adj_vertices(v_id as usize, Some(&label_ids), Direction::Outgoing);
+                Ok(adj_vertices.map(move |v| {
+                    let mut path = vec![];
+                    path.push(v_id);
+                    path.push(v.get_id() as u64);
+                    path
+                }))
+            })?;
+            let (left_partition, right_partition) = left_partition.copied()?;
+            left_partition.key_by(|path| Ok((path[1], path)))?
+            .partition_by_key()
+            .inner_join(right_partition.key_by(|path| Ok((path[1], path)))?.partition_by_key())?
+            .map(|(d1, d2)| {
+                let mut new_path = d1.value;
+                new_path.extend(&d2.value[0..1]);
+                Ok(new_path)
+            })?
+            .sink_into(output)
+        }
+    })
+}
+
+fn _teach_example5(conf: JobConf) -> Result<ResultStream<Vec<u64>>, JobSubmitError> {
+    pegasus::run(conf, move || {
+        move |input, output| {
+            input.input_from(GRAPH.get_all_vertices(None).map(|v| (v.get_id() as u64)))?
+            .flat_map(|v_id| {
+                let label_ids = vec![1];
+                let adj_vertices = GRAPH.get_adj_vertices(v_id as usize, Some(&label_ids), Direction::Outgoing);
+                Ok(adj_vertices.map(move |v| {
+                    let mut path = vec![];
+                    path.push(v_id);
+                    path.push(v.get_id() as u64);
+                    path
+                }))
+            })?
+            .flat_map(|path| {
+                let extend_item_id = path[1];
+                let e_label_ids = vec![1];
+                let adj_vectices = GRAPH.get_adj_vertices(extend_item_id as usize, Some(&e_label_ids), Direction::Incoming);
+                Ok(adj_vectices.map(move |v| {
+                    let mut new_path = path.clone();
+                    new_path.push(v.get_id() as u64);
+                    new_path
+                }))
+            })?
+            .filter(|path| Ok(path[0] != path[2]))?
+            .sink_into(output)
+        }
+    })
+}
